4f3f147cf0eb66364a60d78a3184204461572887
Jeremy Jefferson <jeremyijefferson@gmail.com>
2023-11-01 15:42:18 -0700

    implemented start and pause

--- js/project.js
+++ js/project.js
@@ -1,14 +1,41 @@
 // create initial div to add buttons
 
-// create js function to start timer
-// create js function to pause timer
-// create js function to reset timer
+// create js function for buttons
 
-function onButtonClick() {
-  console.log("button clicked");
-}
+// add event listener to start timer
+// listen for minutes and seconds
 
-document.addEventListener("DOMContentLoaded", () => {
-  const timerStart = document.querySelector(".timer_start");
-  timerStart.addEventListener("click", onButtonClick);
+// add event listener to pause timer
+// pause the time when clicked and resume when clicked again
+
+// add event listener to reset timer
+// reset the timer to 25:00
+
+const timerStart = document.querySelector(".timer_start");
+const pauseTimer = document.querySelector(".timer_pause");
+const resetTimer = document.querySelector(".timer_reset");
+const timerMinutes = document.querySelector(".timer_minutes");
+
+let timerValue = 25;
+let timerInterval = null;
+
+timerStart.addEventListener("click", function () {
+  if (timerInterval === null) {
+    timerInterval = setInterval(function () {
+      timerValue--;
+      timerMinutes.innerHTML = timerValue;
+    }, 60000);
+    timerPausedOnClick();
+  } else {
+    clearInterval(timerInterval);
+    timerInterval = null;
+  }
 });
+
+function timerPausedOnClick() {
+  pauseTimer.addEventListener("click", function () {
+    clearInterval(timerInterval);
+    timerInterval = null;
+    console.log("timer paused");
+  });
+}
