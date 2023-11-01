753ceb6a7675e7ec13d8cc48bc52aa4636b9bd4d
Jeremy Jefferson <jeremyijefferson@gmail.com>
2023-11-01 14:49:15 -0700

    fixed html and js

--- js/project.js
+++ js/project.js
@@ -3,3 +3,12 @@
 // create js function to start timer
 // create js function to pause timer
 // create js function to reset timer
+
+function onButtonClick() {
+  console.log("button clicked");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const timerStart = document.querySelector(".timer_start");
+  timerStart.addEventListener("click", onButtonClick);
+});
