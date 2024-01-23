f014ce4326f756ca2af9f6dc6d9f65873b6ff0fb
Jeremy Jefferson <jeremyijefferson@gmail.com>
2024-01-23 14:58:55 -0800

    Refactor timer functionality and styles

--- js/changeColor.js
+++ js/changeColor.js
@@ -1,7 +1,1 @@
-let isBreakTime = false;
-const breakTimerMinutes = 5;
 
-export function changeBackgroundColor(newColor) {
-  document.documentElement.style.setProperty("--main-bg-color", newColor);
-  console.log("changeBackgroundColor()");
-}
--- js/timer.js
+++ js/timer.js
@@ -1,19 +1,34 @@
-import { changeBackgroundColor } from "./changeColor.js";
+import {
+  changeBackgroundColor,
+  changeMiddleColor,
+  changeTimerColor,
+} from "./changeColor.js";
 
 const timerStart = document.querySelector(".timer_start");
 const pauseTimer = document.querySelector(".timer_pause");
 const resetTimer = document.querySelector(".timer_reset");
 const timerMinutes = document.querySelector(".timer_minutes");
 const timerSeconds = document.querySelector(".timer_seconds");
+const cancelTimer = document.querySelector(".timer_cancel");
 
-let minuteValue = 25;
+let minuteValue = 1;
 let secondValue = 0;
 let timerInterval = null;
+let studyTimeCounter = 0;
+
+let isStudyTimer = true;
 
 function updateTimerDisplay(minutes, seconds) {
   timerMinutes.textContent = String(minutes).padStart(2, "0");
   timerSeconds.textContent = String(seconds).padStart(2, "0");
 }
+
+function incrementStudyTimeCounter() {
+  studyTimeCounter += 1;
+  console.log(studyTimeCounter);
+  localStorage.setItem("studyTimeCounter", studyTimeCounter);
+}
+
 function decrementTimer() {
   if (secondValue === 0) {
     if (minuteValue > 0) {
@@ -21,8 +36,7 @@ function decrementTimer() {
       secondValue = 59;
     } else {
       stopTimer();
-      // Optionally trigger any other action when timer ends
-      //timerEndAction();
+      breakTimer();
       return;
     }
   } else {
@@ -33,6 +47,7 @@ function decrementTimer() {
 
 function startTimer() {
   if (timerInterval === null) {
+    incrementStudyTimeCounter();
     timerInterval = setInterval(decrementTimer, 1000); // set this to 1000 for 1 second
     timerStart.textContent = "Pause";
   }
@@ -44,9 +59,30 @@ function stopTimer() {
   timerStart.textContent = "Start";
 }
 
+function switchTimer() {
+  if (isStudyTimer) {
+    minuteValue = 5;
+    isStudyTimer = false;
+    changeBackgroundColor("blue");
+    changeMiddleColor("lightblue");
+    changeTimerColor("white");
+  } else {
+    minuteValue = 1;
+    isStudyTimer = true;
+  }
+  secondValue = 0;
+  updateTimerDisplay(minuteValue, secondValue);
+}
+
+function cancelTimerFunction() {
+  stopTimer();
+  switchTimer();
+  updateTimerDisplay(minuteValue, secondValue);
+}
+
 function resetTimerFunction() {
   stopTimer();
-  minuteValue = 25;
+  minuteValue = 1;
   secondValue = 0;
   updateTimerDisplay(minuteValue, secondValue);
 }
@@ -59,6 +95,22 @@ timerStart.addEventListener("click", () => {
     stopTimer();
   }
 });
+
+cancelTimer.addEventListener("click", () => {
+  stopTimer();
+  resetTimerFunction();
+});
+
+function breakTimer() {
+  console.log("breakTimer");
+  changeBackgroundColor("blue");
+  changeMiddleColor("lightblue");
+  changeTimerColor("white");
+  minuteValue = 5;
+  secondValue = 0;
+  updateTimerDisplay(minuteValue, secondValue);
+  startTimer();
+}
 
 // Reset button event listener
 resetTimer.addEventListener("click", resetTimerFunction);
